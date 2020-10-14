9dd0be4f39a523a555bc7e531de45e1334729285
Roberto <robertoenrique98@hotmail.com>
2020-10-14 11:35:47 -0600

    Modificación del footer y agregación del logo en el navbar

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  boton: boolean;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private auth: AuthService) { }
 
   ngOnInit(): void {
   }
+
+  mostrar(){
+    if (localStorage.getItem('token')){
+      this.boton = true;
+    } else {
+      this.boton = false;
+    }
+  }
+
+  salir(){
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+  }
+
 
 }
